43dc0f7a1f8519619a0f50fa0446acb6baa4e517
Will Steddin <112439964+willsteddin@users.noreply.github.com>
2022-09-12 19:06:50 +0000

    Fixed the REPL url.

---
+++ ch02/exercises/drawing.py
@@ -0,0 +1,9 @@
+import turtle
+
+sides_num = int(input("Please enter how many sides you would like your shape to have: "))
+sides_len = float(input("Please input how long you would like each side of your shape to be: "))
+print(sides_num, sides_len)
+
+turtle13 = turtle.Turtle()
+turtle.pencolor("palevioletred")
+turtle.fillcolor("palevioletred")
