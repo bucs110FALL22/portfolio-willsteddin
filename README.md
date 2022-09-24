23cf7ac8edca3840fd2b2a8947a0ebf92d97ece2
Will Steddin <112439964+willsteddin@users.noreply.github.com>
2022-09-24 00:55:01 +0000

    Completed Lab 3: Turtle Racing

---
+++ README.md
@@ -0,0 +1,5 @@
+# CS110 Portfolio
+Preferred Name: <<Will Steddin>>
+Discord username: <<BlackWillow69#5679>>
+REPL url: <<https://replit.com/join/bnxwlbotlg-willsteddin>>
+
--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,5 +1,7 @@
 import turtle #1. import modules
 import random
+import pygame
+import math
 
 #Part A
 window = turtle.Screen() # 2.  Create a screen
@@ -11,6 +13,9 @@ michelangelo.color('orange')
 leonardo.color('blue')
 michelangelo.shape('turtle')
 leonardo.shape('turtle')
+michelangelo.speed("slow")
+leonardo.speed("slow")
+
 
 michelangelo.up() # 4. Pick up the pen so we don’t get lines
 leonardo.up()
@@ -19,8 +24,45 @@ leonardo.goto(-100,-20)
 
 ## 5. Your PART A code goes here
 
+michelangelo.fd(random.randrange(0,101))
+leonardo.fd(random.randrange(0,101))
+
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+
+
+for i in [10]*10:
+  michelangelo.fd(random.randrange(0,11))
+  leonardo.fd(random.randrange(0,11))
+  
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+
+window.exitonclick()
 
 # PART B - complete part B here
 
+pygame.init()
+window = pygame.display.set_mode()
 
-window.exitonclick()
+coords = []
+sideLength = 100
+xoffset = 300
+yoffset = 180
+numSides = [3, 4, 6, 9, 360]
+pygame.display.flip()
+pygame.time.wait(2000)
+for _ in numSides:
+  for i in range(_):
+    theta = (2.0 * math.pi * (i/_) )
+    x = sideLength * math.cos(theta) + xoffset
+    y = sideLength * math.sin(theta) + yoffset
+    coords.append((x,y))
+  pygame.display.flip()
+  pygame.draw.polygon(window, "orchid", coords, width=0)
+  pygame.display.flip()
+  pygame.time.wait(1500)
+  window.fill("black")
+  pygame.display.flip()
+  pygame.time.wait(1000)
+  coords = []
