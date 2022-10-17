94c1c74a4ebffe66e157b732e596de554de049d5
willsteddin <wsteddi1@binghamton.edu>
2022-10-17 17:47:07 -0400

    Completed Boolean Functions Exercise

---
+++ ch05/exercises/boolean_functions.py
@@ -0,0 +1,23 @@
+def percentageToLetter(grade):
+    if grade >= 90:
+        return("A")
+    elif grade >= 80 and grade < 90:
+        return("B")
+    elif grade >= 70 and grade < 80:
+        return("C")
+    elif grade >= 60 and grade < 70:
+        return("D")
+    else:
+        return("F")
+
+percentageToLetter(70)
+
+def isPassing(letter):
+    if letter == "A" or letter == "B" or letter == "C":
+        return(True)
+    else:
+        return(False)
+
+gradeNumber = int(input("Enter the grade you received on the exam: "))
+gradeLetter = percentageToLetter(gradeNumber)
+print(isPassing(gradeLetter))
