dd2148b7566afe3a7e94150b5fb2b78faad09ff2
willsteddin <wsteddi1@binghamton.edu>
2022-10-26 18:26:28 -0400

    Began work on midterm (added art matrix)

---
+++ ch06/exam/midterm.py
@@ -0,0 +1,47 @@
+# import turtle
+
+# smb1 = turtle.Turtle()
+
+# numbers = "0#0#0#0#0#0#1#1#1#1#0#0#0#0#0#0"
+# numList = numbers.split("#")
+# print(numList)
+
+# runtime = len(numList)
+# go = 0
+
+# for i in numList:
+#     if i == "0":
+#         pass
+#     elif i == "1":
+#         pass
+#     elif i == "2":
+#         pass
+#     elif i == "3":
+#         pass
+#     else:
+#         print("Unrecognized entry. Please check your input and try again.")
+#         break
+
+#make numList a list of lists
+
+goombaMatrix = [[0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0], [0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0], [0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0], [0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0], [0,0,1,3,3,1,1,1,1,1,1,2,2,1,0,0], [0,1,1,1,2,3,1,1,1,1,3,2,1,1,1,0], [0,1,1,1,2,3,3,3,3,3,3,2,1,1,1,0], [1,1,1,1,2,3,2,1,1,2,3,2,1,1,1,1], [1,1,1,1,2,2,2,1,1,2,2,2,1,1,1,1], [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], [0,1,1,1,1,2,2,2,2,2,2,1,1,1,1,0], [0,0,0,0,2,2,2,2,2,2,2,2,0,0,0,0], [0,0,3,3,2,2,2,2,2,2,2,2,3,3,0,0], [0,3,3,3,3,3,2,2,2,2,3,3,3,3,3,0], [0,3,3,3,3,3,3,2,2,3,3,3,3,3,3,0], [0,0,3,3,3,3,3,0,0,3,3,3,3,3,0,0]]
+
+
+for line in goombaMatrix:
+    for pixel in line:
+        if pixel == 0:
+            pass
+        elif pixel == 1:
+            pass
+        elif pixel == 2:
+            pass
+        elif pixel == 3:
+            pass
+
+
+
+
+
+
+
+
