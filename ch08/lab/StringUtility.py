0c94fd13b9a5554435ffde2c864ad82376b03dfc
willsteddin <wsteddi1@binghamton.edu>
2022-11-12 00:04:28 -0500

    completed ch08 lab

--- ch08/lab/StringUtility.py
+++ ch08/lab/StringUtility.py
@@ -21,3 +21,38 @@ class StringUtility():
         else:
             newstr = self.string[0] + self.string[1] + self.string[len(self.string)-2] + self.string[len(self.string)-1]
             return newstr
+
+    def fixStart(self):
+        repetitions = 0
+        newstr = ""
+        if len(self.string) <= 1:
+            return self.string
+        charOne = self.string[0]
+        for char in self.string:
+            if char == charOne:
+                if repetitions == 0:
+                    newstr += char
+                    repetitions += 1
+                else:
+                    newstr += "*"
+            else: newstr += char
+        return newstr
+
+    def asciiSum(self):
+        sum = 0
+        for char in self.string:
+            sum += ord(char)
+        return sum
+
+    def cipher(self):
+        newstr = ""
+        shift = len(self.string)
+        for char in self.string:
+            if ord(char) == 32:
+                newstr += char
+            if char.isupper() and ord(char) != 32:
+                newstr += chr((ord(char) + shift-65) % 26 +65)
+            elif char.islower() and ord(char) != 32:
+                newstr += chr((ord(char) + shift-97) % 26 +97)
+        return newstr
+
